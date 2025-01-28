bb3e00862d17233957b631d88ecc93f3b349bead
Ievgen Dotsenko <66342665+ievgend2@users.noreply.github.com>
2025-01-28 01:12:30 -0500

    api outdated key and depresiated url fix

--- weather.js
+++ weather.js
@@ -1,13 +1,13 @@
 // Setting up variables
-let cityInput = document.getElementById('city');
-let systemInput
-let infoDiv = document.getElementById('info');
-let infoDiv2 = document.getElementById('info2');
+let cityInput = document.getElementById("city");
+let systemInput;
+let infoDiv = document.getElementById("info");
+let infoDiv2 = document.getElementById("info2");
 
 // API variables
-const searchButton = document.getElementById('search');
-const key = '8f9ed17c9f13a02c8bfa2f65a580eb72';
-const imgUrl = 'http://openweathermap.org/img/wn';
+const searchButton = document.getElementById("search");
+const key = "d89bc06956c9342887b278524be94f53";
+const imgUrl = "http://openweathermap.org/img/wn";
 
 // Show data in the div
 let info_city = document.getElementById("info_city");
@@ -20,44 +20,38 @@ let table_temp_min;
 let table_temp_max;
 let table_weather;
 
-// let today = new Date();
-// let date = +  + (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
-
 Date.prototype.addDays = function (days) {
   let date = new Date(this.valueOf());
   date.setDate(date.getDate() + days);
   return date;
-}
+};
 let date = new Date();
 console.log(date);
 
-
 // add Action on the button
-searchButton.addEventListener('click', (event) => {
-
-  cityInput = document.getElementById('city').value;
+searchButton.addEventListener("click", () => {
+  cityInput = document.getElementById("city").value;
   if (cityInput.length == 0) {
-    alert('Please insert a city name');
+    alert("Please insert a city name");
   } else {
-    systemInput = document.querySelector('input[name="system"]:checked').value
-    // console.log(systemInput)
-    weatherRequest()
+    systemInput = document.querySelector('input[name="system"]:checked').value;
+    weatherRequest();
   }
 });
 
 // Quick Fetch function
-function fetchData(url) {
-  return fetch(url)
-    .then(response => response.json())
+async function fetchData(url) {
+  const response = await fetch(url);
+  return await response.json();
 }
 
-// First Fetch by city inout
+// First Fetch by city input
 function weatherRequest() {
-
-  fetchData(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${key}`)
-
-    .then(data => basicData(data))
-    .catch(notFound)
+  fetchData(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${key}`
+  )
+    .then((data) => basicData(data))
+    .catch(notFound);
 }
 // Display all in web
 function basicData(data) {
@@ -70,27 +64,25 @@ function basicData(data) {
 }
 // Second Fetch with longtitude and latitude from Fetch#1
 async function weatherRequestForecast() {
-  await fetchData(`https://api.openweathermap.org/data/2.5/onecall?lat=${info_lat}&lon=${info_lon}&exclude={part}&units=${systemInput}&appid=${key}`)
-
-
-    .then(data => tableData(data))
+  await fetchData(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${info_lat}&lon=${info_lon}&units=${systemInput}&appid=${key}`
+  ).then((data) => tableData(data));
 }
 
 //  Displaying data in web
 function tableData(data) {
-  const symbol = (systemInput === "imperial" ? ' F' : '°C');
-  table_object = data.daily.map(
+  const symbol = systemInput === "imperial" ? " F" : "°C";
+  table_object = data.list.map(
     (item, i) =>
-
       `<tr>
-      <td>${date.addDays(i).toLocaleString().split(',')[0]}</td>
-      <td>${Math.round(item.temp.min)}${symbol}</td>
-      <td>${Math.round(item.temp.max)}${symbol}</td>
+      <td>${date.addDays(i).toLocaleString().split(",")[0]}</td>
+      <td>${Math.round(item.main.temp_min)}${symbol}</td>
+      <td>${Math.round(item.main.temp_max)}${symbol}</td>
       <td> <img src="${imgUrl}/${item.weather[0].icon}@2x.png"
       title ="${item.weather[0].description}"></th>
     </tr>`
-  )
-  // Create table 
+  );
+  // Create table
   const table = `
   <table class="table bg-dark text-white">
   <thead><h2>Next 7 day's Forecast<h2></thead>
@@ -101,16 +93,15 @@ function tableData(data) {
   <th>Weather</th>
     ${table_object.join("")}
     </tbody>
-  </table>`
+  </table>`;
 
   infoDiv2.innerHTML = table;
 }
 
 // Catch error
 function notFound(error) {
-  var info = document.getElementById('info');
+  var info = document.getElementById("info");
   // console.log(err);
   var newHTML = `<h1>City not found</h1> <p>${error}</p>`;
   info.innerHTML = newHTML;
-  weatherRequest();
 }
